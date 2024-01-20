dbba2990d2fbe4b76a0e4672ac0014bdb04817a9
eduardonba1 <142735616+eduardonba1@users.noreply.github.com>
2024-01-20 16:00:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
 os.system(f"git lfs install")
-os.system(f"git clone -b v1.4 https://github.com/camenduru/ComfyUI /home/xlab-app-center/ComfyUI")
+os.system(f"git clone https://github.com/comfyanonymous/ComfyUI /home/xlab-app-center/ComfyUI")
 os.chdir(f"/home/xlab-app-center/ComfyUI")
 os.system(f"pip install -r requirements.txt")
-os.system(f"git clone -b v1.4 https://github.com/camenduru/ComfyUI-Manager /home/xlab-app-center/ComfyUI/custom_nodes/ComfyUI-Manager")
+os.system(f"git clone https://github.com/ltdrdata/ComfyUI-Manager /home/xlab-app-center/ComfyUI/custom_nodes/ComfyUI-Manager")
 os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.1/RealESRGAN_x2plus.pth -d /home/xlab-app-center/ComfyUI/models/upscale_models -o RealESRGAN_x2plus.pth")
 os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://huggingface.co/ckpt/anything-v4.0/resolve/main/anything-v4.5-pruned.ckpt -d /home/xlab-app-center/ComfyUI/models/checkpoints -o anything-v4.5-pruned.ckpt")
 os.system(f"python main.py --listen 0.0.0.0 --port 7860 --enable-cors-header")
